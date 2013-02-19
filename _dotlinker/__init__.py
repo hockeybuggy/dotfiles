899d582c808cf08fe02fada703b162450474a399
Douglas Anderson <dougjamesanderson@gmail.com>
2013-02-18 23:00:43 -0500

    Made the dotlinker take command line arguments. created an automatic mode. created a clean backups option. I also cleaned a up a bit.

--- _dotlinker/__init__.py
+++ _dotlinker/__init__.py
@@ -1,92 +1,89 @@
 #!/usr/bin/python
 """
 Filename: __init__.py
-Author: Douglas Anderson
-Created: 2013/02/04
+Author  : Douglas Anderson
 
 Reads the config and asks the user for input to decide what files to link
 """
 
-import json
-import os, sys
+import os, sys, re, json
 from _dotlinker.Ask import yes_no_sorta, yes_no
 
 class Linker:
     thisDir = "_dotlinker/"
     configFile = "config.json"
     configPath = os.path.realpath(thisDir + configFile)
-    homePath = os.path.expanduser("~")
-    args = None
+    homeDir = os.path.expanduser("~")
     config = None
 
-    def __init__(self, args):
+    def __init__(self):
         read = open(self.configPath)
         configStr = ""
         for line in read:
             configStr += line.strip()
         self.config = json.loads(configStr)
         read.close()
-        self.args = args
-
-    def link(self):
-        if self.args.interactive:
-            self.interactive_linker()
-        else:
-            self.automatic_linker()
 
     def interactive_linker(self):
-        subGroups = self.config["groups"]
         print "Dotlinker running in interactive mode... Rocking"
-        for group in subGroups:
-            print "\nGroup:", group
-            groupVals = subGroups[group]
-            #print json.dumps(groupVals, indent=4)
-            linkKeys = []
-            linkKeyStr = ""
-            for key in groupVals:
-                if key != "default":
-                    linkKeys.append(key)
-                    linkKeyStr += key + " "
-            print "    ", linkKeyStr
-            ans = yes_no_sorta("Would you like to link this group?", groupVals["default"])
+        groups = self.config["groups"]
+        for groupKey in groups:
+            print "\nGroup:", groupKey
+            group = groups[groupKey]
+            ans = yes_no_sorta("Would you like to link this group?", group["default"])
             if ans == "y":
-                self.link_group(linkKeys, groupVals)
+                self.link_group_gentle(group)
             elif ans == "n":
                 print "Not Linking the Group"
             elif ans == "s":
-                self.link_some(linkKeys, groupVals)
+                self.link_some(group)
 
     def automatic_linker(self):
         print "Dotlinker running in automatic mode... Rocking"
+        groups = self.config["groups"]
+        for groupKey in groups:
+            group = groups[groupKey]
+            if group["default"] == "y":
+                print "Linking", groupKey
+                self.link_group_force(group)
 
-    def link_group(self, keys, group):
+    def get_group_keys(self, group):
+        linkKeys = []
+        linkKeyStr = ""
+        for key in group:
+            if key != "default":
+                linkKeys.append(key)
+                linkKeyStr += key + " "
+        print "\t", linkKeyStr
+        return linkKeys
+
+    def link_group_gentle(self, group):
+        keys = self.get_group_keys(group)
         for key in keys:
-            val = group[key]
-            target_path = os.path.join(self.homePath, val)
+            target_path = os.path.join(self.homeDir, group[key])
             source_path = os.path.join(os.path.realpath(""), key)
             if self.check_for_file(target_path):
-                ans = yes_no(target_path + " already exists. Attempt to move it and create a link?")
-                if ans == "y":
-                    self.move_existing_file(target_path)
-                    self.create_link(source_path, target_path)
-                elif ans == "n":
-                    print "Not Creating a link."
+                self.resolve_conflict(source_path, target_path)
             else:
                 self.create_link(source_path, target_path)
 
-
-    def link_some(self, keys, group):
+    def link_group_force(self, group):
+        keys = self.get_group_keys(group)
         for key in keys:
-            val = group[key]
-            target_path = os.path.join(self.homePath, val)
+            target_path = os.path.join(self.homeDir, group[key])
             source_path = os.path.join(os.path.realpath(""), key)
             if self.check_for_file(target_path):
-                ans = yes_no(target_path + " already exists. Attempt to move it and create a link?")
-                if ans == "y":
-                    self.move_existing_file(target_path)
-                    self.create_link(source_path, target_path)
-                elif ans == "n":
-                    print "Moving existing file or not Creating a link."
+                os.remove(target_path)
+            self.create_link(source_path, target_path)
+
+    def link_some(self, group):
+        keys = self.get_group_keys(group)
+        for key in keys:
+            val = group[key]
+            target_path = os.path.join(self.homeDir, val)
+            source_path = os.path.join(os.path.realpath(""), key)
+            if self.check_for_file(target_path):
+                self.resolve_conflict(source_path, target_path)
             else:
                 ans = Ask.yes_no(target_path + " does not exists. Create a link?", default="y")
                 if ans == "y":
@@ -95,12 +92,21 @@ class Linker:
                 elif ans == "n":
                     print "Not Creating a link."
 
+    def resolve_conflict(self, source, target):
+        ans = yes_no(target + " already exists. Attempt to move it and create a link?")
+        if ans == "y":
+            self.move_existing_file(target)
+            self.create_link(source, target)
+        elif ans == "n":
+            print "Not moving existing file and not Creating a link."
+
     def check_for_file(self, fn):
         if os.path.exists(fn):
             return(True)
         return(False)
 
     def check_for_existing_link(self):
+        #TODO check to see if the file that exists is the file that this would create anyways
         pass
 
     def move_existing_file(self, fn):
@@ -113,8 +119,9 @@ class Linker:
                 sys.exit(1)
         else:
             # This protection is required to protect from the methods silent overwrite
-            print "Error. The file", target, "already exists. Clean up and rerun the program."
+            print "Error. The file", target, "already exists. Clean up .bak files or run with the -c flag."
             sys.exit(1)
+
 
     def create_link(self, source, target):
         #print "SRC", source
@@ -125,4 +132,15 @@ class Linker:
             print "Error. Could not create symlink from", source, "to", target
             sys.exit(1)
 
+    def clean_backups(self):
+        print "Cleaning"
+        done_shit_all = True
+        ex = re.compile(r".*.bak")
+        for fn in os.listdir(self.homeDir):
+            if ex.match(fn):
+                done_shit_all = False
+                print "\tRemoving", fn
+                os.remove(os.path.join(self.homeDir, fn))
+        if done_shit_all:
+            print "\tDid not remove any files during clean"
 
