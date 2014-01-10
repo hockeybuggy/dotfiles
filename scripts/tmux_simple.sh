eb4eed71645105c4d42b479df4b2c16ffe04aae9
Douglas Anderson <dougjamesanderson@gmail.com>
2014-01-10 16:00:03 -0500

    Added a small thang to the tmux start up script. Attaches to the session if it exists

--- scripts/tmux_simple.sh
+++ scripts/tmux_simple.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-tmux new-session -s work -n editor -d
-tmux new-window -t work:3 -n server
+tmux has-session -t work
 
-tmux select-window -t work:1
+if [ $? != 0 ]
+then
+    tmux new-session -s work -n editor -d
+    tmux new-window -t work:3 -n server
+    tmux select-window -t work:1
+fi
+
 tmux -2 attach-session -t work
