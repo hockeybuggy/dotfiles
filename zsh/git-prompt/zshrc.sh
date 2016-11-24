96017c88cbd8112e61f0e7528a2856dd09b5b8c8
Douglas Anderson <hockeybuggy@gmail.com>
2016-11-23 21:03:23 -0500

    Remove the colour work in the zshrc of the git-prompt.

--- zsh/git-prompt/zshrc.sh
+++ zsh/git-prompt/zshrc.sh
@@ -1,5 +1,3 @@
-# To install source this file from your .zshrc file
-
 # Change this to reflect your installation directory
 export __GIT_PROMPT_DIR=~/.zsh/git-prompt
 # Initialize colors.
@@ -25,7 +23,7 @@ function preexec_update_git_vars() {
 }
 
 function precmd_update_git_vars() {
-    if [ -n "$__EXECUTED_GIT_COMMAND" ] || [ -n "$ZSH_THEME_GIT_PROMPT_NOCACHE" ]; then
+    if [ -n "$__EXECUTED_GIT_COMMAND" ]; then
         update_current_git_vars
         unset __EXECUTED_GIT_COMMAND
     fi
@@ -41,56 +39,13 @@ function update_current_git_vars() {
     local gitstatus="$__GIT_PROMPT_DIR/gitstatus.py"
     _GIT_STATUS=`python ${gitstatus}`
     __CURRENT_GIT_STATUS=("${(@f)_GIT_STATUS}")
-    echo __CURRENT_GIT_STATUS
-    GIT_BRANCH=$__CURRENT_GIT_STATUS[1]
-    GIT_REMOTE=$__CURRENT_GIT_STATUS[2]
-    GIT_STAGED=$__CURRENT_GIT_STATUS[3]
-    GIT_CONFLICTS=$__CURRENT_GIT_STATUS[4]
-    GIT_CHANGED=$__CURRENT_GIT_STATUS[5]
-    GIT_UNTRACKED=$__CURRENT_GIT_STATUS[6]
-    GIT_CLEAN=$__CURRENT_GIT_STATUS[7]
+    # echo $__CURRENT_GIT_STATUS
 }
-
 
 git_super_status() {
     precmd_update_git_vars
     if [ -n "$__CURRENT_GIT_STATUS" ]; then
-      STATUS="($GIT_BRANCH"
-      STATUS="$ZSH_THEME_GIT_PROMPT_PREFIX$ZSH_THEME_GIT_PROMPT_BRANCH$GIT_BRANCH%{${reset_color}%}"
-      if [ -n "$GIT_REMOTE" ]; then
-          STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_REMOTE$GIT_REMOTE%{${reset_color}%}"
-      fi
-      STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_SEPARATOR"
-      if [ "$GIT_STAGED" -ne "0" ]; then
-          STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_STAGED$GIT_STAGED%{${reset_color}%}"
-      fi
-      if [ "$GIT_CONFLICTS" -ne "0" ]; then
-          STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_CONFLICTS$GIT_CONFLICTS%{${reset_color}%}"
-      fi
-      if [ "$GIT_CHANGED" -ne "0" ]; then
-          STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_CHANGED$GIT_CHANGED%{${reset_color}%}"
-      fi
-      if [ "$GIT_UNTRACKED" -ne "0" ]; then
-          STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_UNTRACKED%{${reset_color}%}"
-      fi
-      if [ "$GIT_CLEAN" -eq "1" ]; then
-          STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_CLEAN"
-      fi
-      STATUS="$STATUS%{${reset_color}%}$ZSH_THEME_GIT_PROMPT_SUFFIX"
-      echo "$STATUS"
+        STATUS="$__CURRENT_GIT_STATUS"
+        echo "$STATUS"
     fi
 }
-
-# Default values for the appearance of the prompt. Configure at will.
-ZSH_THEME_GIT_PROMPT_PREFIX="("
-ZSH_THEME_GIT_PROMPT_SUFFIX=")"
-ZSH_THEME_GIT_PROMPT_SEPARATOR="|"
-ZSH_THEME_GIT_PROMPT_BRANCH="%{$fg_bold[magenta]%}"
-ZSH_THEME_GIT_PROMPT_STAGED="%{$fg[red]%}●"
-ZSH_THEME_GIT_PROMPT_CONFLICTS="%{$fg[red]%}✖"
-ZSH_THEME_GIT_PROMPT_CHANGED="%{$fg[blue]%}✚"
-ZSH_THEME_GIT_PROMPT_REMOTE=""
-ZSH_THEME_GIT_PROMPT_UNTRACKED="…"
-ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg_bold[green]%}✔"
-
-
